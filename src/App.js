ec95268b6554d3295bf7e5850d7a0ea3873244bd
mathesbh <mathesbh@gmail.com>
2020-12-25 12:35:32 -0300

    Configurando rotas react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import { Link } from 'react-router-dom'
+import { ApplicationRoutes } from './ApplicationRoutes'
 
 export default function App(){
 
   return(
-    <nav>
-      <ul>
-        <li>Início</li>
-        <li>Filmes</li>
-        <li>Séries</li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul>
+          <li><Link to='/'>Início</Link></li>
+          <li>Filmes</li>
+          <li>Séries</li>
+        </ul>
+      </nav>
+      <ApplicationRoutes />
+    </>
   )
 }
---
+++ src/ApplicationRoutes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { Home } from './components/Home'
+
+export const ApplicationRoutes = () => (
+    <Route path="/" exact component={Home} />
+)
