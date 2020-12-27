ed0a7754742769b38098ebd20d9ecaa0d854f5ab
mathesbh <mathesbh@gmail.com>
2020-12-27 09:54:05 -0300

    Configurando componente filmes populares

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ApiService } from '../api/ApiService'
+import { MovieList } from './MovieList'
+import './Movies.css'
 
-export const Movies = () => (
-    <h2>Filmes</h2>
-)
+export default function Movies() {
+    const [movies, setMovies] = useState([])
+
+    const getPopularMovies = async () => {
+        const {
+            data: { results }
+        } = await ApiService.getPopularMovies()
+
+        setMovies(results)
+    }
+    useEffect(() => {
+        getPopularMovies()
+    }, [])
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h2>Filmes populares</h2>
+                    <MovieList movies={movies} />
+                </div>
+            </div>
+        </div>
+    )
+}
