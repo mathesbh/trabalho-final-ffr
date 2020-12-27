953ea7d649036ef0d56a5f6d02140bceaa4ea58d
mathesbh <mathesbh@gmail.com>
2020-12-27 12:01:01 -0300

    Removendo menu mobile

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ export default function App() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <Link className="nav-brand" to='/'><img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="Logo TMDB" width="80" height="80"/></Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item"><Link className="nav-link" to='/filmes'>Filmes</Link></li>
             <li className="nav-item"><Link className="nav-link" to='/series'>Séries</Link></li>
