43ba94ee5eec5e0c83145c3587fe17c4c52f8724
mathesbh <mathesbh@gmail.com>
2020-12-29 14:23:02 -0300

    Teste unitario

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Menu filmes', () => {
+  render(<BrowserRouter>
+    <App />
+  </BrowserRouter>);
+  const menu = screen.getByText('Filmes');
+  expect(menu).toBeInTheDocument();
 });
