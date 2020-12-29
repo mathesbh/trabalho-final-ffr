34d90657bbe97d52aee1542111373b8371d5413e
mathesbh <mathesbh@gmail.com>
2020-12-28 22:12:35 -0300

    Criando lista de favoritos

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,3 +1,26 @@
 import React from 'react'
+import {  useSelector } from 'react-redux'
+import { ButtonRemove } from './ButtonRemove'
 
-export const FavoriteList = () => <h2>Lista favoritos</h2>
+
+export const FavoriteList = () => {
+    const favorites = useSelector(state => state.favorite.favorite) 
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12">
+                    <h2 className="m-4 text-center bg-light text-muted">Meus Favoritos</h2>
+                    <ul className="list-group">
+                        {favorites.map((movie, index) => (
+                            <div key={index} className="list-group-item text-center">
+                                <li className="list-group-item text-muted bg-light">{movie.playload}</li>
+                                <ButtonRemove />
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
